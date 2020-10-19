ff39c00ae8a8a80a9555328e6da2688dc47d9e8f
sebax774 <bugansebastian@gmail.com>
2020-10-19 11:34:12 +0200

    vypracovanie

--- Open-Lab-02.01/Exercise.cs
+++ Open-Lab-02.01/Exercise.cs
@@ -4,14 +4,23 @@ namespace Open_Lab_02._01
 {
     public class Exercise
     {
+
         public double[] Create5Numbers()
         {
-            throw new NotImplementedException();
+            Random rnd = new Random();
+            double[] Create5Numbers = new double[5];
+            Create5Numbers[0] = rnd.Next();
+            Create5Numbers[1] = rnd.Next();
+            Create5Numbers[2] = rnd.Next();
+            Create5Numbers[3] = rnd.Next();
+            Create5Numbers[4] = rnd.Next();
+
+            return Create5Numbers;
         }
 
         public double GetSecond(double[] numbers)
         {
-            throw new NotImplementedException();
+            return numbers[1];
         }
     }
 }
